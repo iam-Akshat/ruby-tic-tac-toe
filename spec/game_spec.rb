eb6e43cef901f85b214ad5cd2e2600e59f390233
Akshat Sethi <masterakshata@gmail.com>
2020-12-04 18:26:31 +0530

    added test for play and ran linter

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,11 +2,16 @@ require_relative '../lib/game'
 require_relative '../lib/player'
 
 describe Game do
-  let(:player1) {Player.new('name1','X')}
-  let(:player2) {Player.new('name2','O')}
-  let(:new_game) {Game.new(player1, player2)}
+  let(:player1) { Player.new('name1', 'X') }
+  let(:player2) { Player.new('name2', 'O') }
+  let(:new_game) { Game.new(player1, player2) }
 
   describe '#play' do
+    it 'does not set change #game_finished unless somewon or draw' do
+      prev_game_finished = new_game.game_finished
+      new_game.play(2)
+      expect(new_game.game_finished).to eql(prev_game_finished)
+    end
     it 'sets value of #game_finished to true when someone wins' do
       new_game.play(1)
       new_game.play(4)
@@ -16,16 +21,16 @@ describe Game do
       expect(new_game.game_finished).to eql(true)
     end
     it "sets value of #game_finished to true when it's a draw" do
-    new_game.play(1)
-    new_game.play(7)
-    new_game.play(8)
-    new_game.play(2)
-    new_game.play(3)
-    new_game.play(9)
-    new_game.play(4)
-    new_game.play(5)
-    new_game.play(6)
-    expect(new_game.game_finished).to eql(true)
+      new_game.play(1)
+      new_game.play(7)
+      new_game.play(8)
+      new_game.play(2)
+      new_game.play(3)
+      new_game.play(9)
+      new_game.play(4)
+      new_game.play(5)
+      new_game.play(6)
+      expect(new_game.game_finished).to eql(true)
     end
   end
 end
