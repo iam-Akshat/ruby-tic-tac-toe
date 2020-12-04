dab63f47e38f26e97038db7e58c1120d2dd59a9c
Akshat Sethi <masterakshata@gmail.com>
2020-12-04 19:59:16 +0530

    added more tests for game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -47,5 +47,35 @@ describe Game do
       new_game.play(1)
       expect(new_game.current_turn.reverse[0]).to_not eql(prev_turn)
     end
+    it 'does not alternate between player turns when invalid input' do
+      prev_turn = new_game.current_turn.reverse[0]
+      new_game.play(50)
+      expect(new_game.current_turn.reverse[0]).to eql(prev_turn)
+    end
+  end
+  describe '#show_board' do
+    it 'prints current board state' do
+      new_game.play(1)
+      expect_board = "\n" + "_Current Board_\n" + "| |X| |2| |3| |\n"\
+      "***************\n" + "| |4| |5| |6| |\n"\
+      "***************\n" + "| |7| |8| |9| |\n"
+      expect(new_game.show_board).to eql(expect_board)
+    end
+  end
+
+  describe '#win_message' do
+    it 'returns nil unless passed a true value' do
+      expect(new_game.win_message(false)).to eql(nil)
+    end
+    it 'does not change win_message_holder unless passed true' do
+      prev_win_message_holder = new_game.win_message_holder
+      new_game.win_message(false)
+      expect(prev_win_message_holder).to eql(new_game.win_message_holder)
+    end
+    it 'sets value of win_message_holder when passed true' do
+      prev_win_message_holder = new_game.win_message_holder
+      new_game.win_message(true)
+      expect(prev_win_message_holder).to_not eql(new_game.win_message_holder)
+    end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,9 @@
 require '../lib/player.rb'
 
 describe Player do
-  
   describe '#initialize' do
-    let(:test_player){Player.new('akshat','X')}
-    
+    let(:test_player) { Player.new('akshat', 'X') }
+
     it 'checks if name was correctly assigned' do
       expect(test_player.name).to eql('akshat')
     end
@@ -12,7 +11,7 @@ describe Player do
       expect(test_player.role).to eql('X')
     end
     it 'assigned X if anything other than X or O is given' do
-      expect(Player.new('akshat','Z').role).to eql('X')
+      expect(Player.new('akshat', 'Z').role).to eql('X')
     end
   end
 end
