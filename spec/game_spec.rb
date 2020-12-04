085a6eb20d4f6782ed7cce5181239c0bffcfbe3f
Mina <cccoldatlas@gmail.com>
2020-12-04 14:50:42 +0200

    test #play

---
+++ spec/game_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../lib/game'
+require_relative '../lib/player'
+
+describe Game do
+  let(:player1) {Player.new('name1','X')}
+  let(:player2) {Player.new('name2','O')}
+  let(:new_game) {Game.new(player1, player2)}
+
+  describe '#play' do
+    it 'sets value of #game_finished to true when someone wins' do
+      new_game.play(1)
+      new_game.play(4)
+      new_game.play(2)
+      new_game.play(5)
+      new_game.play(3)
+      expect(new_game.game_finished).to eql(true)
+    end
+    it "sets value of #game_finished to true when it's a draw" do
+    new_game.play(1)
+    new_game.play(7)
+    new_game.play(8)
+    new_game.play(2)
+    new_game.play(3)
+    new_game.play(9)
+    new_game.play(4)
+    new_game.play(5)
+    new_game.play(6)
+    expect(new_game.game_finished).to eql(true)
+    end
+  end
+end
